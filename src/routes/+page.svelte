5be63f4ed6e7b2d74af7b97a00d0a957142c93cb
jakebogan01 <jakebogan01@gmail.com>
2024-01-15 11:03:44 -0500

    has mobile nav open and closed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,6 +10,7 @@
      console.log($links)
 
      let filterBy = "Active Programs"
+     let showNav = false
 
      $: updatedLinks = $links.filter(link => {
           return link.page.includes(filterBy)
@@ -22,20 +23,22 @@
 </script>
 
 <!--Mobile Nav-->
-<div class="relative z-50 xl:hidden" role="dialog" aria-modal="true">
-     <div class="fixed inset-0 bg-gray-900/80"></div>
-     <div class="fixed inset-0 flex">
-          <div class="relative mr-16 flex w-full max-w-[22rem] flex-1">
-               <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
-                    <button type="button" class="-m-2.5 p-2.5">
-                         <span class="sr-only">Close sidebar</span>
-                         <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
-                    </button>
+{#if showNav}
+     <div class="relative z-50 xl:hidden" role="dialog" aria-modal="true">
+          <div class="fixed inset-0 bg-gray-900/80"></div>
+          <div class="fixed inset-0 flex">
+               <div class="relative mr-16 flex w-full max-w-[22rem] flex-1">
+                    <div class="absolute left-full top-0 flex w-16 justify-center pt-5">
+                         <button type="button" on:click={()=>{showNav = false}} class="-m-2.5 p-2.5">
+                              <span class="sr-only">Close sidebar</span>
+                              <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>
+                         </button>
+                    </div>
+                    <Nav on:click={changeFilterText} {filterBy}/>
                </div>
-               <Nav on:click={changeFilterText} {filterBy}/>
           </div>
      </div>
-</div>
+{/if}
 
 <!--Desktop Nav-->
 <div class="hidden xl:fixed xl:inset-y-0 xl:z-50 xl:flex xl:w-[22rem] xl:flex-col">
@@ -45,7 +48,7 @@
 <!--Main section-->
 <div class="xl:pl-[22rem]">
      <div class="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-6 bg-white px-4 sm:px-6 lg:px-8">
-          <button type="button" class="-m-2.5 p-2.5 text-white xl:hidden">
+          <button type="button" on:click={()=>{showNav = true}} class="-m-2.5 p-2.5 xl:hidden text-[#667979]">
                <span class="sr-only">Open sidebar</span>
                <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true"><path fill-rule="evenodd" d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10zm0 5.25a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75a.75.75 0 01-.75-.75z" clip-rule="evenodd" /></svg>
           </button>
