a76aa1f223b90a93484d46dce0b6776065707cf3
jakebogan01 <jakebogan01@gmail.com>
2024-01-15 13:31:13 -0500

    adds blog posts data

--- src/lib/PnyBlog.svelte
+++ src/lib/PnyBlog.svelte
@@ -1,14 +1,33 @@
+<script>
+    const rssFeedUrl = 'https://blog.pny.com/blogpnycom/rss.xml'
+    const numberOfPosts = 3
+    let rssFeedPosts = []
+    fetch(rssFeedUrl)
+        .then(response => response.text())
+        .then(data => {
+            const parser = new DOMParser()
+            const xmlDoc = parser.parseFromString(data, 'application/xml')
+            const items = xmlDoc.querySelectorAll('item')
+
+            for (let i = 0; i < numberOfPosts && i < items.length; i++) {
+                const item = items[i]
+
+                rssFeedPosts = [...rssFeedPosts, {
+                    title: item.querySelector('title').textContent,
+                    link: item.querySelector('link').textContent
+                }]
+            }
+        })
+        .catch(error => console.error('Error fetching RSS feed:', error))
+</script>
+
 <div class="py-2.5 px-4 border border-[#D3DFE0]">
     <p class="text-sm leading-6 text-[#667979]">PNY Pro Blog</p>
-    <ul class="mt-2 leading-loose text-[#2490C9] text-xs" role="list">
-        <li>
-            <a href="#" target="_blank" class="line-clamp-1">Upcoming Virtual Event: NVIDIA Ada Lovelace Architecture - Performance For Endless Possibilities</a>
-        </li>
-        <li>
-            <a href="#" target="_blank" class="line-clamp-1">NVIDIA Ada Lovelace Architecture: Performance for Endless Possibilities</a>
-        </li>
-        <li>
-            <a href="#" target="_blank" class="line-clamp-1">PNY Offers NVIDIA RTX A5000: Unleashing the Power of Tomorrow’s Innovations</a>
-        </li>
+    <ul class="mt-2 leading-loose text-[#2490C9] text-xs" id="rss-feed-pro-blog" role="list">
+        {#each rssFeedPosts as post}
+            <li>
+                <a href={post?.link} target="_blank" class="line-clamp-1">{post?.title}</a>
+            </li>
+        {/each}
     </ul>
 </div>
