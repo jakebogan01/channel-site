c831224e598e4c6153b146d34bbf52694efe8f0d
jakebogan01 <jakebogan01@gmail.com>
2024-01-15 13:10:41 -0500

    has quick links working

--- src/lib/QuickLinks.svelte
+++ src/lib/QuickLinks.svelte
@@ -1,26 +1,30 @@
+<script>
+    let switchTabs = false
+</script>
+
 <div class="border-l border-b border-t border-[#D3DFE0] bg-[#F7FCFF]">
     <div class="flex">
-        <button type="button" class="w-full py-2 text-xs font-medium leading-6 text-[#667979] bg-[#F7FCFF]">Marketing Links</button>
-        <button type="button" class="w-full py-2 text-xs font-medium leading-6 text-white bg-[#75D0E0]">Sales Links</button>
+        <button type="button" on:click={()=>{switchTabs = false}} class="w-full py-2 text-xs font-medium leading-6 {!switchTabs ? 'text-[#667979] bg-[#F7FCFF]' : 'text-white bg-[#75D0E0]'}">Marketing Links</button>
+        <button type="button" on:click={()=>{switchTabs = true}} class="w-full py-2 text-xs font-medium leading-6 {!switchTabs ? 'text-white bg-[#75D0E0]' : 'text-[#667979] bg-[#F7FCFF]'}">Sales Links</button>
     </div>
     <div class="flex justify-around text-[#2490C9] text-xs">
         <ul class="pl-2 pr-2 pt-6 leading-relaxed" role="list">
             <li>
-                <a href="#" target="_blank">Active Programs</a>
+                <a href="#" target="_blank">{switchTabs ? 'Partner Portal' : 'Active Programs'}</a>
             </li>
             <li>
-                <a href="#" target="_blank">Webinars</a>
+                <a href="#" target="_blank">{switchTabs ? 'Daily Pricing' : 'Webinars'}</a>
             </li>
             <li>
-                <a href="#" target="_blank">Press Center</a>
+                <a href="#" target="_blank">{switchTabs ? 'Corporate Documents' : 'Press Center'}</a>
             </li>
         </ul>
         <ul class="pr-2 pl-2 pt-6 leading-relaxed" role="list">
             <li>
-                <a href="#" target="_blank">Pro Landing</a>
+                <a href="#" target="_blank">{switchTabs ? 'Presentations' : 'Pro Landing'}</a>
             </li>
             <li>
-                <a href="#" target="_blank">Announcements / Newsletters</a>
+                <a href="#" target="_blank">{switchTabs ? 'Pro PowerPoint Template' : 'Announcements / Newsletters'}</a>
             </li>
         </ul>
     </div>
